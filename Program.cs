f9afa19f8774caa053475f4217285667abdfd51e
user <user@Williams-iMac.local>
2020-03-22 21:55:39 +0500

    TestGit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlifLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
